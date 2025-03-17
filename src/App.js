b4ca7845173e77d111b0a0339b19fc9ca0286c3e
calebyhan <calebhantech@gmail.com>
2025-03-17 17:14:08 -0400

    vercel test 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import CameraUI from "./CameraUI";
 import Coding from "./Coding";
 import Photography from "./Photography";
 import About from "./About";
-// import { Analytics } from "@vercel/analytics/react"
+
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
     return (
-        <div className="bg-gray-900 min-h-screen flex justify-center items-center">
-            <Router>
+        <Router>
+            <div className="bg-gray-900 min-h-screen flex justify-center items-center">
                 <Routes>
                     <Route path="/" element={<CameraUI />} />
                     <Route path="/coding" element={<Coding />} />
                     <Route path="/photography" element={<Photography />} />
                     <Route path="/about" element={<About />} />
                 </Routes>
-            </Router>
-            {/*<Analytics />*/}
-        </div>
+            </div>
+            <Analytics />
+            <SpeedInsights />
+        </Router>
     );
 }
 
