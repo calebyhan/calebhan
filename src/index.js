93957dc3252891df4667d87193f54df83a6bcc02
calebyhan <calebhantech@gmail.com>
2025-04-03 14:34:17 -0400

    safari debug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css";
