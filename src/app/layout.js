30efe020536b3de40b730f7422bae44e3a831982
calebyhan <calebhantech@gmail.com>
2026-01-09 11:39:44 -0500

    vercel and google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -14,8 +16,21 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
         <body className="antialiased">
+            <Script
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-SMLM1505P3"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-SMLM1505P3');
+                `}
+            </Script>
             {children}
             <SpeedInsights />
+            <Analytics />
         </body>
         </html>
     );
